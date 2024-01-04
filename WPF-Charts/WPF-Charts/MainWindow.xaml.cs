36fd508e4eb6aa8af2b5ef5379bbff5fa83a04ed
Daniel Miziołek <143220728+Daniel-Miziolek@users.noreply.github.com>
2024-01-04 23:05:57 +0100

    Add files via upload

---
+++ WPF-Charts/WPF-Charts/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.Wpf;
+
+namespace WPF_Charts
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var plotModel = new PlotModel { Title = "Char - Daniel Miziołek" };
+
+            var series = new PieSeries();
+            series.Slices.Add(new PieSlice("Wartość 1", 30) { IsExploded = true });
+            series.Slices.Add(new PieSlice("Wartość 2", 20));
+            series.Slices.Add(new PieSlice("Wartość 3", 50));
+
+            plotModel.Series.Add(series);
+
+            plot.Model = plotModel;
+        }
+    }
+}
