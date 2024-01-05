9cf2ea3a2482f36f358da69680b12ecf9c13229c
Daniel Miziołek <143220728+Daniel-Miziolek@users.noreply.github.com>
2024-01-05 23:26:48 +0100

    Update MainWindow.xaml.cs

--- WPF-Charts/WPF-Charts/MainWindow.xaml.cs
+++ WPF-Charts/WPF-Charts/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using GrapeCity.DataVisualization.Chart;
 using OxyPlot;
 using OxyPlot.Series;
 using OxyPlot.Wpf;
@@ -22,16 +23,97 @@ namespace WPF_Charts
         public MainWindow()
         {
             InitializeComponent();
-            var plotModel = new PlotModel { Title = "Char - Daniel Miziołek" };
+            
+        }
 
-            var series = new PieSeries();
-            series.Slices.Add(new PieSlice("Wartość 1", 30) { IsExploded = true });
-            series.Slices.Add(new PieSlice("Wartość 2", 20));
-            series.Slices.Add(new PieSlice("Wartość 3", 50));
+        private void gotfocus2(object sender, RoutedEventArgs e)
+        {
+            textBox2.Text = "";
+            
+        }
 
-            plotModel.Series.Add(series);
+        private void lostfocus2(object sender, RoutedEventArgs e)
+        {
+            if (textBox2.Text == "")
+            {
+                textBox2.Text = "Value 2>>";
+            }
+            else
+            {
 
-            plot.Model = plotModel;
+            }
+        }
+
+        
+
+        private void lostfocus1(object sender, RoutedEventArgs e)
+        {
+            if (textBox2.Text == "")
+            {
+                textBox2.Text = "Value 1>>";
+            }
+            else
+            {
+
+            }
+        }
+
+        private void gotfocu1(object sender, RoutedEventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (radioButton1.IsChecked == true)
+            {
+                int value = int.Parse(textBox1.Text);
+                int value2 = int.Parse(textBox2.Text);
+                var plotModel = new PlotModel { Title = "chart:" };
+
+                var series = new PieSeries();
+                series.Slices.Add(new PieSlice("Wartość 1", value) { IsExploded = true });
+                series.Slices.Add(new PieSlice("Wartość 2", value2));
+                
+
+                plotModel.Series.Add(series);
+
+                plot.Model = plotModel;
+            }
+            else if (radioButton2.IsChecked == true)
+            {
+                int value = int.Parse(textBox1.Text);
+                int value2 = int.Parse(textBox2.Text);
+                var plotModel = new PlotModel { Title = "chart:" };
+
+                var series = new LineSeries();
+                series.Points.Add(new DataPoint(0, value));
+                series.Points.Add(new DataPoint(1, value2));
+                
+
+                plotModel.Series.Add(series);
+
+                plot.Model = plotModel;
+            }
+            else if (radioButton3.IsChecked == true)
+            {
+                int value = int.Parse(textBox1.Text);
+                int value2 = int.Parse(textBox2.Text);
+                var plotModel = new PlotModel { Title = "chart:" };
+
+                var series = new BarSeries();
+                series.Items.Add(new BarItem { Value = value });
+                series.Items.Add(new BarItem { Value = value2 });
+                
+
+                plotModel.Series.Add(series);
+
+                plot.Model = plotModel;
+            }
+            else
+            {
+                MessageBox.Show("Błąd spróbuj jeszcze raz");
+            }
         }
     }
 }
